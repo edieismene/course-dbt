3bc4c152b11562a0520b453b753ce33478d84119
Edie <isme.nicolaou@gmail.com>
2022-06-15 18:07:43 +0000

    edited source

---
+++ greenery/models/example/staging/stg_addresses.sql
@@ -0,0 +1,22 @@
+with
+
+source as (
+
+    select * from {{ source('public', 'addresses') }}
+
+),
+
+standardized as (
+
+select
+    address_id as user_address_id,
+    address    as user_address,
+    zipcode    as user_zipcode,
+    state      as user_state,
+    country    as user_country
+
+from source
+
+)
+
+select * from standardized

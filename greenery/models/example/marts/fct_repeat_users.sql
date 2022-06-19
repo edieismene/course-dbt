d6c08765ffa13d9383f812bbfdc332af798ea0fb
Edie <isme.nicolaou@gmail.com>
2022-06-19 22:32:35 +0000

    added marts

---
+++ greenery/models/example/marts/fct_repeat_users.sql
@@ -0,0 +1,22 @@
+with user_orders_count as (
+
+    select  user_id
+        ,count(distinct order_id) as order_count
+
+    from   stg_orders
+    group by 1
+
+)
+
+,user_orders_count_buckets as (
+
+    select  user_id
+            ,case when order_count >= 2 then 'true' else 'false' end as repeat_user
+
+    from    user_orders_count
+)
+
+select 
+sum(case when repeat_user = 'true' then 1 else 0 end)::float / count(distinct user_id)::float as user_repeat_rate
+
+from user_orders_count_buckets;
